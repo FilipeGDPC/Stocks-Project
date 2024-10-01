efc11653658cc2621b16c747e94bb3ee0e4748dd
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-01 18:52:34 -0300

    Refactor BaseSideBar component and add menu items dynamically

--- stock-market-project/src/components/BaseSideBar.vue
+++ stock-market-project/src/components/BaseSideBar.vue
@@ -1,13 +1,56 @@
 <template>
-    <div>
-
-    </div>
+  <template v-for="(menuItem, index) in menuList" :key="index">
+    <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+      <q-item-section avatar>
+        <q-icon :name="menuItem.icon" />
+      </q-item-section>
+      <q-item-section>
+        {{ menuItem.label }}
+      </q-item-section>
+    </q-item>
+    <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+  </template>
 </template>
 
 <script setup>
-
+const menuList = [
+  {
+    icon: 'home',
+    label: 'Home',
+    separator: true
+  },
+  {
+    icon: 'paid',
+    label: 'Currencies',
+    separator: false
+  },
+  {
+    icon: 'currency_exchange',
+    label: 'Exchanges',
+    separator: false
+  },
+  {
+    icon: 'error',
+    label: 'Glocal Stock Market',
+    separator: true
+  },
+  {
+    icon: 'settings',
+    label: 'Splits Data',
+    separator: false
+  },
+  {
+    icon: 'feedback',
+    label: 'TimeZones',
+    separator: false
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Help',
+    separator: false
+  }
+]
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
