c024067e39b9057840a23d03e32bb1291f73429a
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-01 10:23:26 -0300

    first commit.

---
+++ stock-market-project/src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+
+import App from './App.vue'
+import router from './router'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(Quasar)
+
+app.mount('#app')
