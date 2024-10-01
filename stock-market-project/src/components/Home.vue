059b3928252ac8340e5820c38aa3291bca076278
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-01 19:01:10 -0300

    Refactor components and add new views
    - Refactor Currencies.vue, Exchanges.vue, GlobalStockMarket.vue, SplitsData.vue, TimeZones.vue, and help.vue components
    - Add new views for Currencies, Exchanges, Global Stock Market, Splits Data, Time Zones, and Help
    - Update router configuration to include new views

--- stock-market-project/src/components/Home.vue
+++ stock-market-project/src/components/Home.vue
@@ -21,7 +21,7 @@
 
     <q-page-container>
       <q-page padding>
-        <p v-for="n in 15" :key="n">
+        <p v-for="n in 1" :key="n">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias
           a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos
           numquam rerum delectus commodi perferendis voluptate?
--- stock-market-project/src/router/index.js
+++ stock-market-project/src/router/index.js
@@ -1,16 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../components/Home.vue'
+import CurrenciesView from '../components/Currencies.vue'
+import ExchangesView from '../components/Exchanges.vue'
+import GlocalStockMarketView from '../components/GlobalStockMarket.vue'
+import SplitsDataView from '../components/SplitsData.vue'
+import TimeZonesView from '../components/TimeZones.vue'
+import HelpView from '../components/help.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/currencies',
+    name: 'currencies',
+    component: CurrenciesView
+  },
+  {
+    path: '/exchanges',
+    name: 'exchanges',
+    component: ExchangesView
+  },
+  {
+    path: '/glocal-stock-market',
+    name: 'glocal-stock-market',
+    component: GlocalStockMarketView
+  },
+  {
+    path: '/splits-data',
+    name: 'splits-data',
+    component: SplitsDataView
+  },
+  {
+    path: '/timezones',
+    name: 'timezones',
+    component: TimeZonesView
+  },
+  {
+    path: '/help',
+    name: 'help',
+    component: HelpView
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
+  routes
 })
 
 export default router
