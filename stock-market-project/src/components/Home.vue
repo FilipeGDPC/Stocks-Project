e4f727309073abfb5bcb29e43fd6cf7170bc4f93
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-04 18:51:42 -0300

    Refactor HomeView component to update welcome message and provide detailed information about the financial data consultation platform

--- stock-market-project/src/components/Home.vue
+++ stock-market-project/src/components/Home.vue
@@ -3,7 +3,7 @@
   <q-layout view="hHh Lpr lff">
     <q-header class="bg-white text-black">
       <q-toolbar>
-        <q-toolbar-title>TradeSmart  <q-icon name="whatshot" /></q-toolbar-title>
+        <q-toolbar-title>TradeSmart <q-icon name="whatshot" /></q-toolbar-title>
       </q-toolbar>
     </q-header>
 
@@ -13,11 +13,25 @@
     </q-drawer>
 
     <q-page-container class="bg-grey-1 ">
-      <q-page padding class="flex-center">
-        <p class="text-center">
-          Welcome to My Stock Market App, here you can find the latest information about the stock market and do the
-          right choice!
-        </p>
+      <q-page padding>
+        <div class="text-center">
+          <p class="text-h4"> Welcome to our financial data consultation platform!</p>
+
+          <p class="text-h6">On our site, you can easily access detailed information about the financial markets with
+            just a few clicks. Powered by the Marketstack APIs, we offer a reliable and user-friendly platform where you
+            can perform free consultations on a wide range of data, including currencies, stock exchanges, time zones,
+            and more.
+          </p>
+
+          <p class="text-h6">We believe that having access to accurate financial data is key to making informed
+            decisions. That’s why we ensure that all queries made through our site are based on precise, up-to-date
+            information from one of the most trusted sources in the industry. Explore and take advantage of having
+            essential financial data like currency rates, exchange details, and global market insights at your
+            fingertips—completely free of charge!
+          </p>
+
+
+        </div>
       </q-page>
     </q-page-container>
   </q-layout>
