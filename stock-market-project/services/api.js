4995660a21da4653792c373b3b47affc278219fb
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-14 15:06:05 -0300

    Refactor api.js file to update currency and stock market APIs and export additional APIs

--- stock-market-project/services/api.js
+++ stock-market-project/services/api.js
@@ -28,7 +28,7 @@ const tickersApi = axios.create({     // This is the API that we will use to get
     },
 });
 
-const globalstockmarket = axios.create({
+const historicaldate = axios.create({    
     baseURL: 'https://api.marketstack.com/v1/eod?symbols=AAPL,MSFT,GOOGL,AMZN,TSLA,FB,NVDA,BRK.B,JNJ,V,UNH,WMT,PG,JPM,MA,HD,PFE,BAC,DIS,VZ,KO,NFLX,INTC,CSCO,NKE,PEP,XOM,MRK,ABBV,T,LLY,AVGO,ADBE,PYPL,ORCL,CRM,CMCSA,ABT,QCOM,AMD,BMY,CVS,GILD,AMGN,BA,HON,CAT,MMM,MDT,C,RTX&date_from=2024-02-26&date_to=2024-03-07&access_key=635c2dc4ba2b1148d3132d1482ae9b23',
     headers: {
         'Content-Type': 'application/json',
@@ -36,4 +36,4 @@ const globalstockmarket = axios.create({
 });
 
 
-export default {currenciesApi, exchangesApi, timezonesApi, tickersApi, globalstockmarket};
+export default {currenciesApi, exchangesApi, timezonesApi, tickersApi, historicaldate};
