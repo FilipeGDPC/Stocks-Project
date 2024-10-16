33dbe165c178a074bdd0090b50aceef77d0bc636
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-16 09:00:11 -0300

    Refactor Home.vue to update welcome and description texts

--- stock-market-project/src/components/Home.vue
+++ stock-market-project/src/components/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="text-center text-white q-pa-md">
-    <p class="text-h4 q-mb-md">Welcome to our financial data consultation platform!</p>
+    <p class="text-h4 q-mb-md welcome-text">Welcome to our financial data consultation platform!</p>
 
-    <p class="text-h6 q-mb-md">On our site, you can easily access detailed information about the financial markets with
+    <p class="text-h6 q-mt-xl description-text">On our site, you can easily access detailed information about the financial markets with
       just a few clicks. Powered by the Marketstack APIs, we offer a reliable and user-friendly platform where you
       can perform free consultations on a wide range of data, including currencies, stock exchanges, time zones,
       and more.
     </p>
 
-    <p class="text-h6 q-mb-md">We believe that having access to accurate financial data is key to making informed
+    <p class="text-h6 q-mb-md description-text">We believe that having access to accurate financial data is key to making informed
       decisions. That’s why we ensure that all queries made through our site are based on precise, up-to-date
       information from one of the most trusted sources in the industry. Explore and take advantage of having
       essential financial data like currency rates, exchange details, and global market insights at your
       fingertips—completely free of charge!
     </p>
-
+    <hr>
     <div class="q-mt-xl">
       <q-btn color="black" label="Get Started" @click="showModal = true" />
     </div>
@@ -58,5 +58,15 @@ const goToRoute = (routeName) => {
 <style scoped>
 .text-white {
   color: white;
+}
+.welcome-text {
+  font-weight: 700;
+  font-size: 2rem;
+}
+
+.description-text {
+  font-weight: 800;
+  font-size: 1.2rem;
+  line-height: 1.6;
 }
 </style>
