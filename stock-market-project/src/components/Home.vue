e506ad1bf7bb0602fbde670e2807f233ca0b54b4
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-18 13:07:49 -0300

    Refactor Home.vue to update CSS classes in the welcome text and description sections

--- stock-market-project/src/components/Home.vue
+++ stock-market-project/src/components/Home.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="text-center text-black q-mx-auto q-my-lg" style="max-width: 700px;">
-    <p class="text-h4 welcome-text text-bold">Welcome to our financial data consultation platform!</p>
+  <q-card class="q-mx-auto q-mt-xl" style="max-width: 750px; height: 600px;">
+    <q-card-section>
+      <div class="q-pa-md">
+        <p class="text-h4 text-center welcome-text text-bold q-mb-xl">Welcome to our financial data consultation
+          platform!</p>
 
-    <p class="text-h6 q-mt-xl description-text">On our site, you can easily access detailed information about the financial markets with
-      just a few clicks. Powered by the Marketstack APIs, we offer a reliable and user-friendly platform where you
-      can perform free consultations on a wide range of data, including currencies, stock exchanges, time zones,
-      and more.
-    </p>
+        <p class="text-h6 description-text">On our site, you can easily access detailed information about the financial
+          markets with
+          just a few clicks. Powered by the Marketstack APIs, we offer a reliable and user-friendly platform where you
+          can perform free consultations on a wide range of data, including currencies, stock exchanges, time zones,
+          and more.
+        </p>
 
-    <p class="text-h6 q-mb-md description-text">We believe that having access to accurate financial data is key to making informed
-      decisions. That’s why we ensure that all queries made through our site are based on precise, up-to-date
-      information from one of the most trusted sources in the industry. Explore and take advantage of having
-      essential financial data like currency rates, exchange details, and global market insights at your
-      fingertips completely free of charge!
-    </p>
-  </div>
+        <p class="text-h6 q-mt-lg description-text">We believe that having access to accurate financial data is key to
+          making informed
+          decisions. That’s why we ensure that all queries made through our site are based on precise, up-to-date
+          information from one of the most trusted sources in the industry.
+        </p>
+
+        <p class="text-h6 q-mt-lg description-text">Explore and take advantage of having
+          essential financial data like currency rates, exchange details, and global market insights at your
+          fingertips completely free of charge!</p>
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup>
