331b186d053d721496b30bd801389fe02b8fecee
Filipe Cordeiro <filipegama2411@gmail.com>
2024-10-18 18:06:38 -0300

    Refactor help.vue to update CSS classes in the welcome text and description sections

--- stock-market-project/src/components/help.vue
+++ stock-market-project/src/components/help.vue
@@ -1,12 +1,52 @@
 <template>
-   <div class="text-center text-black q-mx-auto q-my-lg">
-    <p class="text-h4 text-bold">Help</p>
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
- 
-  </style>
+  <div>
+    <q-card class="q-mx-auto q-mt-xl" style="max-width: 550px; height: 300px;">
+      <q-card-section>
+        <div class="q-pa-md">
+          <p class="text-h4 text-center welcome-text text-bold q-mb-xl">Do you need help? Get in contact with us!</p>
+          <div class="q-mt-lg text-center">
+            <q-btn
+              flat
+              round
+              color="primary"
+              icon="mdi-instagram"
+              @click="openLink('https://www.instagram.com/filipegdev/')"
+              class="q-mr-md"
+            />
+            <q-btn
+              flat
+              round
+              color="primary"
+              icon="mdi-github"
+              @click="openLink('https://github.com/FilipeGDPC')"
+              class="q-mr-md"
+            />
+            <q-btn
+              flat
+              round
+              color="primary"
+              icon="mdi-email"
+              @click="openLink('mailto:filipegama2411@gmail.com')"
+            />
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+const openLink = (url) => {
+  window.open(url, '_blank');
+};
+</script>
+
+<style scoped>
+.text-bold {
+  font-weight: bold;
+}
+
+.welcome-text {
+  font-size: 2rem;
+}
+</style>
